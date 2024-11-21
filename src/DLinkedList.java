64614cfd2223bdcb0b9ff5bc1a68f7f08c29374d
Daniel Thompson <dthompson1@unm.edu>
2024-11-21 14:33:36 -0700

    implement a part of linked list

--- src/DLinkedList.java
+++ src/DLinkedList.java
@@ -1,5 +1,26 @@
 public class DLinkedList {
+    Node root;
     public DLinkedList(){
-
+        this.root = new Node();
+    }
+    public void add(Object value){
+        Node curr = this.root;
+        Node addedNode;
+        while(curr.getNext() != null){
+            curr = curr.getNext();
+        }
+        addedNode = new Node(value,curr);
+        curr.setNext(addedNode);
+    }
+    public Object get(int index){
+        Node curr = this.root;
+        while(curr.getNext() != null){
+            if(curr.getIndex() == index){
+                return curr.getValue();
+            }
+            Node nextNode = curr.getNext();
+            curr = nextNode;
+        }
+        return null;
     }
 }
--- src/Node.java
+++ src/Node.java
@@ -1,5 +1,36 @@
 public class Node {
-    public Node(){
+    private Object value;
+    private Node next;
+    private Node prev;
+    private int index;
+        public Node(){
+        this.value = null;
+        this.next = null;
+        this.prev = null;
+        this.index = -1;
 
+    }
+    public Node(Object value,Node prev){
+        this.value = value;
+        this.prev = prev;
+        this.next = null;
+        this.index = prev.index+1;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+    public int getIndex(){
+        return this.index;
+    }
+    public Object getValue(){
+        return this.value;
+    }
+    public Node getNext(){
+            return this.next;
     }
 }
