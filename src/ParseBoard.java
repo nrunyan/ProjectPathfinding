d6c694d1e264cbde37f726e71e3b924276fd2945
Natalie Runyan <nrunyan@unm.edu>
2024-11-22 15:26:14 -0700

    Djstras
    # Conflicts:
    #	.gitignore
    #	out/production/ProjectPathfinding/BreadFirst.class
    #	out/production/ProjectPathfinding/ParseBoard.class

--- src/ParseBoard.java
+++ src/ParseBoard.java
@@ -1,10 +1,7 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class ParseBoard {
-    private List<List<Integer>> adjecency=new ArrayList<>();
+    public List<List<Integer>> adjecency=new ArrayList<>();
     private int amountOfNodes;
     private int colunmSize;
     private int rowSize;
